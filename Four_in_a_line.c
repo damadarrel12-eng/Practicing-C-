58a454d15a14c77bd96692f9e14c3016981d3960
Darrel <damadarrel12@gmail.com>
2026-09-19 11:14:19 +0100

    Optimizing modifications made

--- Four_in_a_line.c
+++ Four_in_a_line.c
@@ -6,6 +6,7 @@
 
 typedef char Tboard[ROWS][COLS];
 
+#include"aligned.h"
 
 void display_grid(Tboard A){
     int i,j;
@@ -21,15 +22,14 @@ void display_grid(Tboard A){
         printf("\n");
     }
 }
-int drop_disc(A, col, disc){
+int drop_disc( Tboard A,int col,char disc){
     int done = 0;
-    Tboard A;
-    int col;
-    int disc;
-    int i = ROWS;
+    
+    int i = ROWS-1;
     if(col>=1 && col<=COLS){
-        while( done !=1 && i>0){
-            if( A[i][col] = ''){
+        col--;
+        while( done !=1 && i>=0){
+            if( A[i][col] == ' '){
                 A[i][col] =disc;
                 done = 1;
             }
@@ -40,49 +40,55 @@ int drop_disc(A, col, disc){
     }
     return done;
 }
-int play_game(){
+void play_game(){
     int stop = 0;
     int placed_discs = 0;
     int player = 0;
-    char emptygrid(c4grid);
-    
+    Tboard c4grid;
+    for(int i =0;i<ROWS;i++){
+        for(int j=0;j<COLS;j++){
+            c4grid[i][j] = ' ';
+        }
+    }
+    int disc;
+    int col;
     do{
         if(player%2 == 0){
             printf("Player 1,PLAY(Enter X/O)\n");
-            int disc = 'X';
+            disc = 'X';
             scanf("%d", &col );
-            player++;
+            
             }else{
             printf("Player 2,PLAY(Enter X/O)");
-            int disc = 'O';
+            disc = 'O';
             scanf("%d", &col);
-            player++;
+        
+
             }
-        int done = drop_disc(c4grid,col,disc)
-        if(done = 1){
+        int done = drop_disc( c4grid, col, disc);
+        if(done == 1){
         display_grid(c4grid);
         player++;
         placed_discs++;
-        }else if{
+        }else if(done ==0){
         printf("Chosen column is full");
         }else{
         printf("Invalid column number");
         }  
         int aligned= is_aligned(c4grid);
+
+
         if(aligned == 1 && disc == 'X'){
             printf("Game Over.Player 1 Wins!");
             stop = 1;
-        }else if( is_aligned == 1 && disc == 'O'){
+        }else if( aligned == 1 && disc == 'O'){
             printf("Game Over!.Player 2 Wins");
             stop=1;
-        }else{
-            printf("Error, Unknown Player type: %d !", disc);
-
         }
     
 
-    }while(stop !=1 && placed_discs < 25)
-    if(stop=0){
+    }while(stop !=1 && placed_discs < 25); 
+    if(stop==0){
         printf("Game Over! It's a tie");
 
     }
@@ -90,12 +96,8 @@ int play_game(){
 
 
 int main(){
-    Tboard A;
-    int col;
-    int disc;
-    Tboard c4grid;
-    display_grid(c4grid);
-    play_game()
+    
+    play_game();
 
 
 
